751da306beffa4f2948cbc7885c37451a1406f12
Tim Huynh <thuynh@interactive.com.au>
2017-09-10 20:38:48 +1000

    done

--- Python/Python Fundamentals/strings.py
+++ Python/Python Fundamentals/strings.py
@@ -1,1 +1,22 @@
-print "hello world"
+words = "It's thanksgiving day. It's my birthday,too!"
+
+print words.find("day")
+
+words2 = words.replace("day", "month", 1)
+
+print words2
+
+x = [2, 54, -2, 7, 12, 98]
+print min(x)
+print max(x)
+x = ["hello", 2, 54, -2, 7, 12, 98, "world"]
+print x[0]
+print x[len(x)-1]
+x = [19, 2, 54, -2, 7, 12, 98, 32, 10, -3, 6]
+x.sort()
+y = []
+for num in range(len(x)/2):
+    y.append(x[num])
+print y
+x[0] = y
+print x
