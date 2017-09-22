bedcee98aa052097e5327153aaf0ac11ca3216a4
Tim <timhuynh53@gmail.com>
2017-09-22 17:28:29 +1000

    Done

--- Python/Flask/Disappearing Ninja/disappearingninja.py
+++ Python/Flask/Disappearing Ninja/disappearingninja.py
@@ -2,42 +2,26 @@ from flask import Flask, render_template, request, redirect, session
 import random, datetime
 app = Flask(__name__)
 
+ninja_dict = {
+    'purple': 'images/donatello.jpg',
+    'blue': 'images/leonardo.jpg',
+    'red': 'images/raphael.jpg',
+    'orange': 'images/michelangelo.jpg'
+}
 app.secret_key = 'bcxjnmkbnhbfrvsghfn'
 
 @app.route('/')
 def index():
+    return render_template("index.html")
 
-    return render_template("index.html", )
-
-@app.route('/ninjas')
+@app.route('/ninjas/')
 def ninjas():
-
-    if session.get('ninjas') is None:
-        session['ninjas'] = []
-    session['ninjas'].append({'name': 'images/donatello.jpg'})
-    session['ninjas'].append({'name':'images/leonardo.jpg'})
-    session['ninjas'].append({'name':'images/michelangelo.jpg'})
-    session['ninjas'].append({'name':'images/raphael.jpg'})
-    return render_template("ninjas.html", ninjas = session['ninjas'])
-
-
+    return render_template("ninjas.html", ninjas = ninja_dict)
 
 @app.route('/ninjas/<vararg>')
 def handler_function(vararg):
-    session['ninjas'] = []
-    if vararg == 'orange':
-        session['ninjas'].append({'name': 'images/michelangelo.jpg'})
-    elif vararg == 'blue':
-        session['ninjas'].append({'name': 'images/leonardo.jpg'})
-    elif vararg == 'red':
-        session['ninjas'].append({'name': 'images/raphael.jpg'})
-    elif vararg == 'purple':
-        session['ninjas'].append({'name': 'images/donatello.jpg'})
-    else:
-        session['ninjas'].append({'name': 'images/notapril.jpg'}) 
+    url = ninja_dict.get(vararg, 'images/notapril.jpg')
 
-
-    return render_template("ninjas.html", ninjas = session['ninjas'])
-
+    return render_template("ninjas.html", ninjas = {'chosen': url})
 
 app.run(debug=True)
